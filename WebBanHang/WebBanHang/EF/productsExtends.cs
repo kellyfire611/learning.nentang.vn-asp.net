d1edca6601d2f9796db30f2ad849a85e5137c060
kellyfire611@gmail.com <kellyfire611@gmail.com>
2020-01-15 20:50:25 +0700

    Add method get url from Azure Storage

---
+++ WebBanHang/WebBanHang/EF/productsExtends.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//Add The following Namespaces to the project
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob; // Multipart
+
+namespace WebBanHang.EF
+{
+    public partial class products
+    {
+        public string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=kellyfire611;AccountKey=1231321231231321313;EndpointSuffix=core.windows.net";
+
+        public string GetImageUrlFromAzureStore()
+        {
+            string blobUrl = "";
+
+            // Update file lên thư mục Storage Azure
+            // Create Reference to Azure Storage Account
+            String strorageconn = this.StorageConnectionString;
+            CloudStorageAccount storageacc = CloudStorageAccount.Parse(strorageconn);
+
+            //Create Reference to Azure Blob
+            CloudBlobClient blobClient = storageacc.CreateCloudBlobClient();
+
+            //The next 2 lines create if not exists a container named "democontainer"
+            CloudBlobContainer container = blobClient.GetContainerReference("democontainer");
+
+            //The next 7 lines upload the file test.txt with the name DemoBlob on the container "democontainer"
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(this.image); // hoahong.jpg
+            blobUrl = blockBlob.Uri.AbsoluteUri; //https://azure.storage.com/kellyfire/democontainer/hoahong.jpg
+
+            return blobUrl;
+        }
+    }
+}
